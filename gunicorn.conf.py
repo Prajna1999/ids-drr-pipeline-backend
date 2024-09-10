c580ef825fa82ef49bb450f552b1501654883951
Prajna1999 <gituprajna20@gmail.com>
2024-09-10 16:26:25 +0530

    FIX: reduce the number of worker processes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,14 +1,7 @@
-import multiprocessing
-workers = multiprocessing.cpu_count()*2+1
+workers = 4
 
 bind = "0.0.0.0:8080"
 
-try:
-    import gevent
-    worker_class = "gevent"
-except ImportError:
-    worker_class = "sync"
+worker_class = "gevent"
 
 timeout = 120
-
-loglevel = "info"
